09c46eb07b11268bea114b27c317bec8b97de469
Nicomarino <nicolasmarino1985@gmail.com>
2024-08-24 18:36:23 -0300

    Cierre pre entrega 1

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-let nombreUsuario = prompt ("Ingrese su nombre");
-console.log(nombreUsuario);
+// Bienvenida al sitio web
+let primeraVisita = !localStorage.getItem('bienvenidaMostrada');
 
-alert("Hola " + nombreUsuario + " Bienvenido al sitio de Viveros el Botanico ")
+while (primeraVisita) {
+    // Muestra el mensaje de bienvenida
+    let nombreUsuario = prompt("Ingrese su nombre");
+    console.log(nombreUsuario);
 
+    alert("Hola " + nombreUsuario + "\nBienvenido a Viveros el Botánico");
 
+    // Guarda la información en localStorage para que no vuelva a mostrarse
+    localStorage.setItem('bienvenidaMostrada', 'true');
+
+    // Asegura que el bucle no se ejecute de nuevo
+    primeraVisita = false;
+}
 //MENU
 let menuVisible = false;
 //Función que oculta o muestra el menu
@@ -24,8 +34,6 @@ function seleccionar() {
     nav.classList.remove("responsive");
     menuVisible = false;
 }
-
-
 // SLIDER
 let slideIndex = 0;
 mostrarSlide();
